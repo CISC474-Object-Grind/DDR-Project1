a1c5cb4b568833236cde132d02b6e57053832053
Alyssa Burks <burksan@udel.edu>
2020-03-02 22:15:13 -0500

    everything up to date

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-var DDRUI=function(){
-    var self = this;
-    this.game = undefined;
-    this.running = false;
-    this.initialize = function(){
-
-            $('#GameStopped').show();
-            $('GameRunning').hide();
-
-            $('#StartBtn').on('click',function(){
-                $('#GameStopped').hide();
-                $('#GameRunning').show();
-                self.running = true;
-            });
-            $('#StopBtn').on('click',function(){
-                $('#GameStopped').show();
-                $('#GameRunning').hide();
-                self.running = false;
-                self.game.reset();
-            });
-
-    }
-
-    this.initialize();
-}
-=======
 var DDRUI = function () {
 	var self = this;
 	this.game = undefined;
@@ -50,4 +23,3 @@ var DDRUI = function () {
 	};
 	this.initialize();
 }
->>>>>>> be20d5745659f4863e0ab803b4b4c7a9b9bc9b7d
