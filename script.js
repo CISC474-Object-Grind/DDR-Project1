be20d5745659f4863e0ab803b4b4c7a9b9bc9b7d
Emily Evans <emilyevans@wifi-roaming-128-4-107-109.host.udel.edu>
2020-03-02 22:11:12 -0500

    got the button to appear and disappear

---
+++ script.js
@@ -0,0 +1,25 @@
+var DDRUI = function () {
+	var self = this;
+	this.game = undefined;
+	this.running = false;
+	this.initialize = function () {
+	
+		$('#GameStopped').show();
+		$('#GameRunning').hide();
+		$('#DanceFloor').hide();
+
+		$('#StartBtn').on('click', function () {
+			$('#GameStopped').hide();
+			$('#GameRunning').show();
+			self.running = true;
+		});
+		$('#StopBtn').on('click', function () {
+			$('#GameStopped').show();
+			$('#GameRunning').hide();
+			self.running = false;
+			// self.game.reset();
+			// self.refreshView();
+		});
+	};
+	this.initialize();
+}
